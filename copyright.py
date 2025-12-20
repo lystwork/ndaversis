bc1beb82401ad5219f8a17e4cd8f06d0e2d12fa9
lystwork <59729727+lystwork@users.noreply.github.com>
2025-12-20 22:42:46 +0100

    Update copyright.py

--- copyright.py
+++ copyright.py
@@ -1,4 +1,4 @@
-CONTACT_EMAIL = "n@ndaotec.com"
-COPYRIGHT_HOLDER = "Nikita Andreevich Drozdov"
-REPOSITORY_ADDRESS = "github.com/lystwork/ndaversis"
-COPYRIGHT_TEXT = "ndaotec.com. @ All rights reserved - Nikita Andreevich Drozdov. All rights belong to their respective owners."
+CONTACT EMAIL = "n@ndaotec.com"
+COPYRIGHT HOLDER = "Nikita Andreevich Drozdov"
+REPOSITORY ADDRESS = "github.com/lystwork/ndaversis"
+COPYRIGHT TEXT = "ndaotec.com. @ All rights reserved - Nikita Andreevich Drozdov. All rights belong to their respective owners."
